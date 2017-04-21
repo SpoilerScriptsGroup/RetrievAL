bc501fc00f66998d3d9349b068bbb253b3e5387e
cron <>
2017-04-22 06:00:02 +0900

    Sat Apr 22 05:25:06 JST 2017

--- diff/GetFileTitlePointerT.c
+++ diff/GetFileTitlePointerT.c
@@ -9,24 +9,11 @@ TCHAR * __fastcall GetFileTitlePointerT(const TCHAR *lpFileNeme)
 	{
 		do
 		{
+			if (*lpFileNeme == TEXT('\\') || *lpFileNeme == TEXT(':'))
+				lpTitle = (TCHAR *)lpFileNeme + 1;
 #ifndef _UNICODE
-			if (!IsDBCSLeadByte(*lpFileNeme))
-			{
-#endif
-				switch (*lpFileNeme)
-				{
-				case TEXT('\\'):
-				case TEXT(':'):
-					lpTitle = (TCHAR *)lpFileNeme + 1;
-					break;
-				}
-#ifndef _UNICODE
-			}
-			else
-			{
-				if (!*(++lpFileNeme))
-					break;
-			}
+			else if (IsDBCSLeadByte(*lpFileNeme) && !*(++lpFileNeme))
+				break;
 #endif
 		} while (*(++lpFileNeme));
 	}

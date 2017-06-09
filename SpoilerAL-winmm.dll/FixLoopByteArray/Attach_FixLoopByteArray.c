b5a430fec9c3aa136cec69c7ca6e7fe63dada0e7
cron <>
2017-06-10 03:00:02 +0900

    Sat Jun 10 02:57:52 JST 2017

--- SpoilerAL-winmm.dll/FixLoopByteArray/Attach_FixLoopByteArray.c
+++ SpoilerAL-winmm.dll/FixLoopByteArray/Attach_FixLoopByteArray.c
@@ -16,11 +16,12 @@ EXTERN_C void Attach_FixLoopByteArray()
 	*(LPDWORD)0x00506F7D = NOP_X4;
 
 	// TSSGCtrl::StrToProcessAccessElementVec
-	*(LPBYTE )0x0050B4D4 = JMP_REL32;
-	*(LPDWORD)0x0050B4D5 = (DWORD)Caller_ParsingWithRel - (0x0050B4D5 + sizeof(DWORD));
-
-	// TSSGCtrl::StrToProcessAccessElementVec
+	*(LPBYTE )(0x0050AEF8 + 1) = 0x00;
 	*(LPBYTE )0x0050AF9C = JMP_REL32;
 	*(LPDWORD)0x0050AF9D = (DWORD)Caller_FixLoopByteArray - (0x0050AF9D + sizeof(DWORD));
 	*(LPBYTE )0x0050AFA1 = NOP;
+
+	// TSSGCtrl::StrToProcessAccessElementVec
+	*(LPBYTE )0x0050B4D4 = JMP_REL32;
+	*(LPDWORD)0x0050B4D5 = (DWORD)Caller_ParsingWithRel - (0x0050B4D5 + sizeof(DWORD));
 }

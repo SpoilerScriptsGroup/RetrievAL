dfa9013746787713256a7354faa5e6c567a0688f
cron <>
2017-09-24 10:00:02 +0900

    Sun Sep 24 09:11:35 JST 2017

--- SpoilerAL-winmm.dll/crt/tcstod.h
+++ SpoilerAL-winmm.dll/crt/tcstod.h
@@ -26,55 +26,128 @@ double __cdecl _tcstod(const TCHAR *nptr, TCHAR **endptr)
 		while (*p == ' ' || (*p <= (UCHAR)'\r' && *p >= (UCHAR)'\t'))
 			p++;
 
-		if ((sign = (*p == '-')) || *p == '+')
+		if ((sign = *p == '-') || *p == '+')
 			p++;
-
-		nptr = (const TCHAR *)p;
 
 		if (p[0] != '0' || (p[1] != 'x' && p[1] != 'X'))
 		{
+			#define MAX_EXPONENT 511
+
+			const UCHAR  *decptr;
+			unsigned int size, exp;
+
+			decptr = p;
 			while ((SCHAR)*p >= '0' && *p <= (UCHAR)'9')
-				r = r * 10 + (*(p++) - '0');
+				p++;
+			size = exp = p - decptr;
 
 			if (*p == '.')
 			{
-				double d;   /* scale */
-
-				d = 1;
-				while ((SCHAR)*(++p) >= '0' && *p <= (UCHAR)'9')
-					r += (d *= 0.1) * (*p - '0');
+				while ((SCHAR)*(++p) >= '0' && *p <= (UCHAR)'9');
+				size = p - decptr - 1;
 			}
 
-			if (p != (const UCHAR *)nptr && (*p == 'e' || *p == 'E'))
+			if (p != decptr)
 			{
-				bool esign;
+				unsigned int i;
+				const UCHAR  *expptr;
 
-				if ((esign = (*(++p) == '-')) || *p == '+')
-					p++;
+				p = decptr;
 
-				if ((SCHAR)*p >= '0' && *p <= (UCHAR)'9')
+				if (size > 18)
+					size = 18;
+
+				exp -= size;
+
+				i = 0;
+				for (; size > 9; size--)
 				{
-					unsigned int e; /* exponent */
+					if (*p == '.')
+						p++;
+					i = 10 * i + (*(p++) - '0');
+				}
+				r = 1e9 * i;
+				if (size)
+				{
+					i = 0;
+					do
+					{
+						if (*p == '.')
+							p++;
+						i = 10 * i + (*(p++) - '0');
+					} while (--size);
+					r += i;
+				}
 
-					e = *(p++) - '0';
-					while ((SCHAR)*p >= '0' && *p <= (UCHAR)'9')
-						e = e * 10 + (*(p++) - '0');
+				expptr = p;
+				if (*p == 'e' || *p == 'E')
+				{
+					bool esign;
 
-					if (esign)
-						while (e--)
-							r *= 0.1;
+					if ((esign = *(++p) == '-') || *p == '+')
+						p++;
+					if ((SCHAR)*p >= '0' && *p <= (UCHAR)'9')
+					{
+						i = 0;
+						do
+						{
+							i = i * 10 + (*(p++) - '0');
+						} while ((SCHAR)*p >= '0' && *p <= (UCHAR)'9');
+						exp += (esign ? -(int)i : i);
+					}
 					else
-						while (e--)
-							r *= 10;
+					{
+						p = expptr;
+						goto DONE;
+					}
+				}
+
+				if (exp)
+				{
+					bool esign;
+
+					if (esign = (int)exp < 0)
+						exp = -(int)exp;
+
+					if (exp <= MAX_EXPONENT)
+					{
+						static const double powers[] = { 1e1, 1e2, 1e4, 1e8, 1e16, 1e32, 1e64, 1e128, 1e256 };
+
+						double       d;
+						const double *pwr;
+
+						d = 1;
+						pwr = powers;
+						do
+						{
+							if (exp & 1)
+								d *= *pwr;
+							pwr++;
+						} while (exp >>= 1);
+						if (esign)
+							r /= d;
+						else
+							r *= d;
+					}
+					else
+					{
+						errno = ERANGE;
+						r = HUGE_VAL;
+					}
 				}
 			}
+			else
+			{
+				p = (const UCHAR *)nptr;
+			}
 
-			if (MSW(r) == 0x7FEFFFFF && LSW(r) > 0xFFFFFFF8)
-				r = DBL_MAX;
+			#undef MAX_EXPONENT
 		}
 		else
 		{
-			p += 2;
+			const UCHAR *decptr;
+
+			decptr = p += 2;
 
 			for (; ; )
 			{
@@ -117,44 +190,52 @@ double __cdecl _tcstod(const TCHAR *nptr, TCHAR **endptr)
 				}
 			}
 
-			if (p != (const UCHAR *)nptr && (*p == 'p' || *p == 'P'))
+			if (p != decptr)
 			{
-				bool esign;
-
-				if ((esign = (*(++p) == '-')) || *p == '+')
-					p++;
-
-				if ((SCHAR)*p >= '0' && *p <= (UCHAR)'9')
+				if (*p == 'p' || *p == 'P')
 				{
-					unsigned int e; /* exponent */
+					bool esign;
 
-					e = *(p++) - '0';
-					while ((SCHAR)*p >= '0' && *p <= (UCHAR)'9')
-						e = e * 10 + (*(p++) - '0');
-					if (esign)
-						e = -(int)e;
-					e += MSW(r) >> 20;
-					MSW(r) &= 0x000FFFFF;
-					if ((int)e > 0)
+					if ((esign = (*(++p) == '-')) || *p == '+')
+						p++;
+
+					if ((SCHAR)*p >= '0' && *p <= (UCHAR)'9')
 					{
-						if (e < 0x7FF)
-							MSW(r) |= e << 20;
+						unsigned int e; /* exponent */
+
+						e = *(p++) - '0';
+						while ((SCHAR)*p >= '0' && *p <= (UCHAR)'9')
+							e = e * 10 + (*(p++) - '0');
+						if (esign)
+							e = -(int)e;
+						e += MSW(r) >> 20;
+						MSW(r) &= 0x000FFFFF;
+						if ((int)e > 0)
+						{
+							if (e < 0x7FF)
+								MSW(r) |= e << 20;
+							else
+								r = HUGE_VAL;
+						}
 						else
-							r = HUGE_VAL;
-					}
-					else
-					{
-						MSW(r) |= 0x00100000;
-						e = -(int)e + 1;
-						if (e <= 52)
-							*(unsigned __int64 *)&r >>= e;
-						else
-							*(unsigned __int64 *)&r = 0;
+						{
+							MSW(r) |= 0x00100000;
+							e = -(int)e + 1;
+							if (e <= 52)
+								*(unsigned __int64 *)&r >>= e;
+							else
+								*(unsigned __int64 *)&r = 0;
+						}
 					}
 				}
 			}
+			else
+			{
+				p = (const UCHAR *)nptr;
+			}
 		}
 
+	DONE:
 		if ((MSW(r) >> 20) == 0x7FF)
 			errno = ERANGE;
 

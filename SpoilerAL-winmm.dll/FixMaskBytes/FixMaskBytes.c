5bb456cff0baf28ecd972692938fbc95cf84d3e2
cron <>
2018-01-24 03:00:02 +0900

    Wed Jan 24 02:17:19 JST 2018

--- SpoilerAL-winmm.dll/FixMaskBytes/FixMaskBytes.c
+++ SpoilerAL-winmm.dll/FixMaskBytes/FixMaskBytes.c
@@ -5,13 +5,12 @@ EXTERN_C void __cdecl FixMaskBytes()
 {
 	// TSSGCtrl::StrToProcessAccessElementVec
 	/*
-		lea     eax, [esi + 2]                          ; 0050C6ED _ 8D. 46, 02
+		lea     eax, [esi + 2]  ; 0050C6ED _ 8D. 46, 02
 	*/
 	*(LPDWORD)(0x0050C6EC) = BSWAP32(0xFF83C002);
 
 	/*
-		lea     ecx, [esi + 2]                          ; 0050C702 _ 8D. 4E, 02
+		lea     ecx, [esi + 2]  ; 0050C702 _ 8D. 4E, 02
 	*/
-	*(LPWORD )(0x0050C702) = BSWAP16(0x8D4E);
-	*(LPBYTE )(0x0050C704) =         0x02;
+	*(LPDWORD)(0x0050C700) = BSWAP32(0xFFFF8D4E);
 }

7cd07f08927f203c3c56baeaf3634e5e4d492361
cron <>
2018-02-04 03:00:03 +0900

    Sun Feb  4 02:38:48 JST 2018

--- SpoilerAL-winmm.dll/FixMaskBytes/FixMaskBytes.c
+++ SpoilerAL-winmm.dll/FixMaskBytes/FixMaskBytes.c
@@ -7,7 +7,7 @@ EXTERN_C void __cdecl FixMaskBytes()
 	/*
 		lea     eax, [esi + 2]  ; 0050C6ED _ 8D. 46, 02
 	*/
-	*(LPDWORD)(0x0050C6EC) = BSWAP32(0xFF83C002);
+	*(LPDWORD)(0x0050C6EC) = BSWAP32(0xFF8D4602);
 
 	/*
 		lea     ecx, [esi + 2]  ; 0050C702 _ 8D. 4E, 02

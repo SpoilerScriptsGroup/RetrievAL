246f628f6c8560deac79580dd8e9047e402cc977
sceawung <sceawung@users.noreply.github.com>
2018-07-07 17:58:32 +0900

    Fix masking bits

--- SpoilerAL-winmm.dll/FixListFEP/Attach_FixListFEP.c
+++ SpoilerAL-winmm.dll/FixListFEP/Attach_FixListFEP.c
@@ -1,4 +1,51 @@
 #include <windows.h>
+#include "TSSGSubject.h"
+
+#pragma pack(push, 1)
+typedef struct _TSSBitList {
+	TSSGSubject     grandParent;
+	bcb6_std_string lockStr;
+	BYTE            size;
+	BYTE            padding[7];
+	bcb6_std_string addressStr;
+	bcb6_std_string indexFileName;
+	bcb6_std_string realFileName;
+	unsigned long   maskByte;
+} TSSBitList;
+#pragma pack(pop)
+
+void __stdcall ReplaceDefineDynamic(TSSGSubject* SSGS, bcb6_std_string* line);
+
+static void __fastcall TSSBitList_Setting_maskByteCalc(TSSBitList* SSBL, bcb6_std_vector_string* ListFile) {
+	unsigned long mask = -1LL << SSBL->size * 8;
+	size_t End = bcb6_std_vector_size_by_type(ListFile, bcb6_std_string);
+	for (size_t i = 0; i < End; ++i) {
+		bcb6_std_string* line = &bcb6_std_vector_type_at(ListFile, bcb6_std_string, i);
+		//ReplaceDefineDynamic((TSSGSubject*)SSBL, line);
+		if (!bcb6_std_string_empty(line))
+			mask |= 1 << i;
+	}
+	SSBL->maskByte = ~mask;
+}
+
+static void __declspec(naked) TSSBitList_Setting_maskByteStub() {
+	__asm {
+		mov  edx, edi
+		mov  ecx, ebx
+		push 0x004B86E2
+		jmp  TSSBitList_Setting_maskByteCalc
+	}
+}
+
+static void __declspec(naked) TSSBitList_Write_ValStub() {
+	__asm {
+		mov   eax, dword ptr [ebp - 0x0220]// Src
+		and   eax, dword ptr [ebp - 0x0094]// MaskByte
+		mov   dword ptr [ebp - 0x0090], eax// Val
+		movzx ecx, byte ptr [ebx + 0x78]   // TSSBitList->size
+		ret
+	}
+}
 
 EXTERN_C void __cdecl TSSBitList_Setting_CheckFEPParam();
 EXTERN_C void __cdecl TSSBundleList_Setting_CheckFEPParam();
@@ -9,6 +56,8 @@ EXTERN_C void __cdecl TSSGCtrl_OneWrite_with_CheckIO_FEP();
 
 #define NOP        (BYTE)0x90
 #define CALL_REL32 (BYTE)0xE8
+#define JMP_REL32  (BYTE)0xE9
+#define OR         (BYTE)0x09
 
 EXTERN_C void __cdecl Attach_FixListFEP()
 {
@@ -17,11 +66,21 @@ EXTERN_C void __cdecl Attach_FixListFEP()
 	*(LPDWORD)0x004B8353 = (DWORD)TSSBitList_Setting_CheckFEPParam - (0x004B8353 + sizeof(DWORD));
 	*(LPBYTE )0x004B8357 = NOP;
 
+	*(LPBYTE )0x004B8632 = JMP_REL32;
+	*(LPDWORD)0x004B8633 = (DWORD)TSSBitList_Setting_maskByteStub - (0x004B8633 + sizeof(DWORD));
+
 	// TSSBitList::Read
 	*(LPDWORD)(0x004B910F + 1) = (DWORD)TSSGCtrl_OneRead_with_CheckIO_FEP - (0x004B910F + 1 + sizeof(DWORD));
 
 	// TSSBitList::Write
+	*(LPDWORD) 0x004BB488 = 0xFF70BD8B;// mov edi, dword ptr [ebp-90h]
+	*(LPDWORD) 0x004BB48C = 0xAB0FFFFF;// bts edi, eax  
+	*(LPWORD ) 0x004BB490 =     0x89C7;// mov dword ptr [ebp-90h], edi
 	*(LPDWORD)(0x004BB522 + 1) = (DWORD)TSSGCtrl_OneRead_with_CheckIO_FEP - (0x004BB522 + 1 + sizeof(DWORD));
+	*(LPBYTE ) 0x004BB5D7 = CALL_REL32;
+	*(LPDWORD)(0x004BB5D7 + 1) = (DWORD)TSSBitList_Write_ValStub - (0x004BB5D7 + 1 + sizeof(DWORD));
+	*(LPBYTE ) 0x004BB6A7 = OR;
+	*(LPBYTE ) 0x004BB614 = OR;
 	*(LPDWORD)(0x004BB6F9 + 1) = (DWORD)TSSGCtrl_OneWrite_with_CheckIO_FEP - (0x004BB6F9 + 1 + sizeof(DWORD));
 
 	// TSSBundleList::Setting

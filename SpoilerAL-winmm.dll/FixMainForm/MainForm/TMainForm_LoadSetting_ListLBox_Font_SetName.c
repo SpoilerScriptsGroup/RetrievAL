15366096db3eae4fea5430919b759d58b32e451a
sceawung <sceawung@users.noreply.github.com>
2018-07-22 18:54:22 +0900

    Set NoteREdit's font to LockCBox and ToggleCBox

--- SpoilerAL-winmm.dll/FixMainForm/MainForm/TMainForm_LoadSetting_ListLBox_Font_SetName.c
+++ SpoilerAL-winmm.dll/FixMainForm/MainForm/TMainForm_LoadSetting_ListLBox_Font_SetName.c
@@ -4,20 +4,29 @@ __declspec(naked) void __cdecl TMainForm_LoadSetting_ListLBox_Font_SetName()
 	__asm
 	{
 		#define this                        edi
-		#define offsetof_TMainForm_MultiLBox 3E4H
 		#define offsetof_TMainForm_ListLBox 3E8H
-		#define offsetof_TListBox_Font      68H
+		#define offsetof_TControl_Font      68H
 		#define TFont_SetName               0055D578H
 
 		push    eax
 		push    edx
-		mov     eax, dword ptr [this + offsetof_TMainForm_MultiLBox]
-		mov     eax, [eax + offsetof_TListBox_Font]
+		mov     eax, dword ptr [this + 0x03D0]// LockCBox
+		mov     eax, [eax + offsetof_TControl_Font]
+		mov     ecx, TFont_SetName
+		call    ecx
+		mov     edx, [esp]
+		mov     eax, dword ptr [this + 0x03DC]// ToggleCBox
+		mov     eax, [eax + offsetof_TControl_Font]
+		mov     ecx, TFont_SetName
+		call    ecx
+		mov     edx, [esp]
+		mov     eax, dword ptr [this + 0x03E4]// MultiLBox
+		mov     eax, [eax + offsetof_TControl_Font]
 		mov     ecx, TFont_SetName
 		call    ecx
 		mov     edx, [esp]
 		mov     eax, dword ptr [this + offsetof_TMainForm_ListLBox]
-		mov     eax, [eax + offsetof_TListBox_Font]
+		mov     eax, [eax + offsetof_TControl_Font]
 		mov     ecx, TFont_SetName
 		call    ecx
 		pop     edx
@@ -27,8 +36,7 @@ __declspec(naked) void __cdecl TMainForm_LoadSetting_ListLBox_Font_SetName()
 
 		#undef this
 		#undef offsetof_TMainForm_ListLBox
-		#undef offsetof_TMainForm_MultiLBox
-		#undef offsetof_TListBox_Font
+		#undef offsetof_TControl_Font
 		#undef TFont_SetName
 	}
 }

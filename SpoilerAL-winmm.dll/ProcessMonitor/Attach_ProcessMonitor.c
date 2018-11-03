70e1b06dc87f9d400b0310917946cb5d79163e3d
sceawung <sceawung@users.noreply.github.com>
2018-11-04 03:50:03 +0900

    Refines progress drawing

--- SpoilerAL-winmm.dll/ProcessMonitor/Attach_ProcessMonitor.c
+++ SpoilerAL-winmm.dll/ProcessMonitor/Attach_ProcessMonitor.c
@@ -28,6 +28,49 @@ static void __declspec(naked) __cdecl TSearchForm_AdjustValToString_GetStart(LPV
 	}
 }
 
+static void __declspec(naked) __fastcall TBrush_setColor(LPVOID this, COLORREF color) {
+	__asm {
+		mov eax, ecx
+		mov ecx, 0x0055DB3C
+		jmp ecx
+	}
+}
+
+static void __declspec(naked) __fastcall TCanvas_FillRect(LPVOID this, RECT* rect) {
+	__asm {
+		mov eax, ecx
+		mov ecx, 0x0055E1D4
+		jmp ecx
+	}
+}
+
+static void __fastcall TSearchForm_DrawCanvas(struct TProcessSearchReportListnerBase {
+	LPVOID*       vftable;
+	unsigned long reserveMode;
+	void*         thisPointer;
+	unsigned long reserveVal;
+	unsigned long reportSpan;
+	unsigned long start, min, max, step;
+} *reportListner, long ImageWidth, unsigned long Pos, struct TCanvas {
+	BYTE   unknown[0x14];
+	LPVOID Brush;
+} *Canv) {
+	RECT rect  = { 0, 0, ImageWidth, 24 };
+	long denom = (reportListner->max - reportListner->min) / ImageWidth;
+	TBrush_setColor(Canv->Brush, 0x000000);
+	if (Pos >= reportListner->start) {
+		TCanvas_FillRect(Canv, &rect);
+		rect.left = (reportListner->start - reportListner->min) / denom;
+		rect.right = (Pos - reportListner->min) / denom;
+	} else {
+		rect.right = (reportListner->start - reportListner->min) / denom;
+		TCanvas_FillRect(Canv, &rect);
+		rect.left = (Pos - reportListner->min) / denom;
+	}
+	TBrush_setColor(Canv->Brush, 0x008000);
+	TCanvas_FillRect(Canv, &rect);
+}
+
 #define JMP_REL32 (BYTE )0xE9
 #define NOP_X4    (DWORD)0x90909090
 
@@ -48,6 +91,15 @@ EXTERN_C void __cdecl Attach_ProcessMonitor()
 	*(LPDWORD)0x004946F5 = (DWORD)TSearchForm_AdjustValToString_GetStart - (0x004946F5 + sizeof(DWORD));
 
 	// TSearchForm::DrawProgress
+#ifndef USE_ORIGINAL
+	*(LPBYTE )0x0049492D =       0x56;
+	*(LPWORD )0x0049492E =     0x8B8D;
+	*(LPDWORD)0x00494930 = 0x00000400;
+	*(LPDWORD)0x00494934 = 0xE80C75FF;
+	*(LPDWORD)0x00494938 = (DWORD)TSearchForm_DrawCanvas - (0x00494938 + sizeof(DWORD));
+	*(LPBYTE )0x0049493C =  JMP_REL32;
+	*(LPDWORD)0x0049493D = 0x00494B39 - (0x0049493D + sizeof(DWORD));
+#else
 	//	activeElement->GetStart(*ssgCtrl) => reportListner.start
 	*(LPBYTE )0x00494941 =       0x8D;
 	*(LPWORD )0x00494942 =     0x008B;
@@ -64,6 +116,7 @@ EXTERN_C void __cdecl Attach_ProcessMonitor()
 	*(LPDWORD)0x00494A3C = 0x04008B8D;
 	*(LPDWORD)0x00494A40 = 0xEC830000;
 	*(LPDWORD)0x00494A44 = 0x1C418B04;
+#endif
 
 	// TSearchForm::SerachStopToolBtnClick
 	//	if(searchMode==0) return; => if(searchMode!=1 || reportListner.reserveMode==prcsCtrl::rsEND) return;
@@ -92,4 +145,7 @@ EXTERN_C void __cdecl Attach_ProcessMonitor()
 	//	missing Listner->OnSearchEnd()
 	*(LPBYTE )0x004A6B16 = JMP_REL32;
 	*(LPDWORD)0x004A6B17 = 0x004A6D9E - (0x004A6B17 + sizeof(DWORD));
+	//	missing Listner->OnSearchEnd()
+	*(LPBYTE )0x004A6C73 = JMP_REL32;
+	*(LPDWORD)0x004A6C74 = 0x004A6B9A - (0x004A6C74 + sizeof(DWORD));
 }

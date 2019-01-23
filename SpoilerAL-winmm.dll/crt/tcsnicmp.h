065a9121443042fc9dc79dd7a25c117df6ccff67
cron <>
2019-01-24 02:00:02 +0900

    Thu Jan 24 01:50:00 JST 2019
      Content-Type: application/zip
      Content-Length: 828329
      Date: Wed, 23 Jan 2019 17:00:01 GMT
      Accept-Ranges: bytes
      Server: LiteSpeed
      Alt-Svc: quic=":443"; ma=2592000; v="35,37,38,39"
      Connection: Keep-Alive

--- SpoilerAL-winmm.dll/crt/tcsnicmp.h
+++ SpoilerAL-winmm.dll/crt/tcsnicmp.h
@@ -19,76 +19,78 @@
 #ifndef _M_IX86
 int __cdecl _tcsnicmp(const TCHAR *string1, const TCHAR *string2, size_t count)
 {
-	if (count)
-		do
-		{
-			TCHAR c1, c2;
+	string1 += count;
+	string2 += count;
+	count = ~count;
+	while (++count)
+	{
+		TCHAR c1, c2;
 
-			c1 = *(string1++);
-			c2 = *(string2++);
-			if (!(c1 -= c2))
-			{
+		c1 = string1[count];
+		c2 = string2[count];
+		if (!(c1 -= c2))
+		{
 #ifdef _MBCS
-				if (!c2)
-					break;
-				if (!IsDBCSLeadByteEx(CP_THREAD_ACP, c2))
-					continue;
+			if (!c2)
+				break;
+			if (!IsDBCSLeadByteEx(CP_THREAD_ACP, c2))
+				continue;
 #ifdef _MBSNBICMP
-				if (--count)
+			if (++count)
 #endif
-				{
-					c1 = *(string1++);
-					c2 = *(string2++);
-					if (c1 == c2)
-					{
-#endif
-						if (!c2)
-							break;
-#ifdef _MBCS
-					}
-				}
-#endif
-			}
-			else
 			{
-				if (c1 == 'A' - 'a')
-				{
-#ifdef _UNICODE
-					if ((short)c2 >= 'a' && c2 <= 'z')
-#else
-					if ((char)c2 >= 'a' && c2 <= 'z')
+				c1 = string1[count];
+				c2 = string2[count];
+				if (!(c1 -= c2))
 #endif
+					if (c2)
 						continue;
-				}
-				else if (c1 == 'a' - 'A')
-				{
-#ifdef _UNICODE
-					if ((short)c2 >= 'A' && c2 <= 'Z')
-#else
-					if ((char)c2 >= 'A' && c2 <= 'Z')
-#endif
-						continue;
-				}
-				c1 += c2;
+					else
+						break;
+#ifdef _MBCS
 			}
-#ifdef _UNICODE
-			return (int)c1 - (int)c2;
-#else
-			return (int)(unsigned char)c1 - (int)(unsigned char)c2;
 #endif
-		} while (--count);
+		}
+		else
+		{
+			if (c1 == 'A' - 'a')
+			{
+#ifdef _UNICODE
+				if ((short)c2 >= 'a' && c2 <= 'z')
+#else
+				if ((char)c2 >= 'a' && c2 <= 'z')
+#endif
+					continue;
+			}
+			else if (c1 == 'a' - 'A')
+			{
+#ifdef _UNICODE
+				if ((short)c2 >= 'A' && c2 <= 'Z')
+#else
+				if ((char)c2 >= 'A' && c2 <= 'Z')
+#endif
+					continue;
+			}
+		}
+		c1 += c2;
+#ifdef _UNICODE
+		return (int)c1 - (int)c2;
+#else
+		return (int)(unsigned char)c1 - (int)(unsigned char)c2;
+#endif
+	}
 	return 0;
 }
 #else
 __declspec(naked) int __cdecl _tcsnicmp(const TCHAR *string1, const TCHAR *string2, size_t count)
 {
 #ifdef _UNICODE
+	#define sizeof_tchar 2
 	#define tchar_ptr    word ptr
-	#define inc_tchar(r) add r, 2
 	#define t(r)         r##x
 #else
+	#define sizeof_tchar 1
 	#define tchar_ptr    byte ptr
-	#define inc_tchar(r) inc r
 	#define t(r)         r##l
 #endif
 
@@ -97,85 +99,89 @@ __declspec(naked) int __cdecl _tcsnicmp(const TCHAR *string1, const TCHAR *strin
 		#define string1 (esp + 4)
 		#define string2 (esp + 8)
 		#define count   (esp + 12)
+#ifndef _MBCS
+		#define cnt ecx
+#else
+		#define cnt edi
+#endif
 
-		mov     eax, dword ptr [count]
 		push    ebx
-		test    eax, eax
-		jz      L5
 		push    esi
-		push    edi
+#ifndef _MBCS
+		push    cnt
+#endif
 		mov     ebx, dword ptr [string1 + 12]
 		mov     esi, dword ptr [string2 + 12]
-		mov     edi, eax
-		sub     esi, ebx
+		mov     cnt, dword ptr [count + 12]
+#ifdef _UNICODE
+		lea     ebx, [ebx + cnt * sizeof_tchar]
+		lea     esi, [esi + cnt * sizeof_tchar]
+#else
+		add     ebx, cnt
+		add     esi, cnt
+#endif
+		xor     cnt, -1
 
 		align   16
 	L1:
-		mov     t(c), tchar_ptr [ebx]
-		xor     eax, eax
-		mov     t(a), tchar_ptr [ebx + esi]
-		inc_tchar(ebx)
-		sub     t(c), t(a)
-		jnz     L6
+		inc     cnt
+		jz      L2
+		mov     t(a), tchar_ptr [ebx + cnt * sizeof_tchar]
+		mov     t(d), tchar_ptr [esi + cnt * sizeof_tchar]
+		sub     t(a), t(d)
+		jnz     L3
 #ifdef _MBCS
-		test    t(a), t(a)
-		jz      L4
-		push    eax
+		test    t(d), t(d)
+		jz      L2
+		push    ecx
 		push    CP_THREAD_ACP
 		call    IsDBCSLeadByteEx
 		test    eax, eax
+		jz      L1
+#ifdef _MBSNBICMP
+		inc     cnt
 		jz      L2
-#ifdef _MBSNBICMP
-		dec     edi
-		jz      L3
 #endif
-		mov     t(c), tchar_ptr [ebx]
-		xor     eax, eax
-		mov     t(a), tchar_ptr [ebx + esi]
-		inc_tchar(ebx)
-		cmp     t(c), t(a)
-		jne     L9
+		mov     t(a), tchar_ptr [ebx + cnt * sizeof_tchar]
+		mov     t(d), tchar_ptr [esi + cnt * sizeof_tchar]
+		cmp     t(a), t(d)
+		jne     L6
 #endif
-		test    t(a), t(a)
-		jz      L4
-	L2:
-		dec     edi
+		test    t(d), t(d)
 		jnz     L1
-#ifdef _MBSNBICMP
-	L3:
-#endif
+	L2:
 		xor     eax, eax
-	L4:
-		pop     edi
+#ifdef _MBCS
+		pop     cnt
+#endif
 		pop     esi
-	L5:
 		pop     ebx
 		ret
 
 		align   16
-	L6:
-		cmp     t(c), 'A' - 'a'
-		je      L7
-		cmp     t(c), 'a' - 'A'
-		jne     L8
-		cmp     t(a), 'A'
-		jl      L8
-		cmp     t(a), 'Z'
-		jbe     L2
-		jmp     L8
+	L3:
+		cmp     t(a), 'A' - 'a'
+		je      L4
+		cmp     t(a), 'a' - 'A'
+		jne     L5
+		cmp     t(d), 'A'
+		jl      L5
+		cmp     t(d), 'Z'
+		jbe     L1
+		jmp     L5
 
 		align   16
-	L7:
-		cmp     t(a), 'a'
-		jl      L8
-		cmp     t(a), 'z'
-		jbe     L2
-	L8:
-		add     t(c), t(a)
+	L4:
+		cmp     t(d), 'a'
+		jl      L5
+		cmp     t(d), 'z'
+		jbe     L1
+	L5:
+		add     t(a), t(d)
 #ifdef _MBCS
-	L9:
+	L6:
+		pop     cnt
 #endif
-		pop     edi
 		sbb     eax, eax
 		pop     esi
 		or      eax, 1
@@ -184,6 +190,11 @@ __declspec(naked) int __cdecl _tcsnicmp(const TCHAR *string1, const TCHAR *strin
 
 		#undef string1
 		#undef string2
+		#undef count
+		#undef cnt
 	}
+	#undef sizeof_tchar
+	#undef tchar_ptr
+	#undef t
 }
 #endif

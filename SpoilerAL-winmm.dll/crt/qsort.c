fb6f6aa690214bb7c4da6253abb4964c1ab8a59b
cron <>
2019-12-19 21:00:07 +0900

    Thu Dec 19 20:40:25 JST 2019
      Content-Type: application/zip
      Last-Modified: Thu, 19 Dec 2019 11:40:25 GMT
      Accept-Ranges: bytes
      Content-Length: 957140
      Date: Thu, 19 Dec 2019 12:00:01 GMT
      Server: LiteSpeed
      Alt-Svc: quic=":443"; ma=2592000; v="39,43,46", h3-Q039=":443"; ma=2592000, h3-Q043=":443"; ma=2592000, h3-Q046=":443"; ma=2592000, h3-23=":443"; ma=2592000, h3-24=":443"; ma=2592000

--- SpoilerAL-winmm.dll/crt/qsort.c
+++ SpoilerAL-winmm.dll/crt/qsort.c
@@ -6,14 +6,14 @@ INDEX
 ANSI_SYNOPSIS
 	#include <stdlib.h>
 	void qsort(void *<[base]>, size_t <[num]>, size_t <[width]>,
-	           int (*<[compare]>)(const void *, const void *));
+	           int(__cdecl *<[compare]>)(const void *, const void *));
 TRAD_SYNOPSIS
 	#include <stdlib.h>
 	qsort(<[base]>, <[num]>, <[width]>, <[compare]>)
-	char *<[base]>;
+	void *<[base]>;
 	size_t <[num]>;
 	size_t <[width]>;
-	int (*<[compare]>)();
+	int(__cdecl *<[compare]>)(const void *, const void *);
 DESCRIPTION
 <<qsort>> sorts an array (beginning at <[base]>) of <[num]> objects.
 <[width]> describes the size of each element of the array.
@@ -67,6 +67,8 @@ PORTABILITY
  */
 
 #include <stdlib.h>
+#include <stdint.h>
+#include <stdbool.h>
 
 #ifndef min
 #define min(a, b) (((a) < (b)) ? (a) : (b))
@@ -75,42 +77,36 @@ PORTABILITY
 /*
  * Qsort routine from Bentley & McIlroy's "Engineering a Sort Function".
  */
-#define swapcode(TYPE, a, b, size)      \
-do {                                    \
-    size_t __n = (size) / sizeof(TYPE); \
-    register TYPE *__pa = (TYPE *)(a);  \
-    register TYPE *__pb = (TYPE *)(b);  \
-    do {                                \
-        register TYPE __t = *__pa;      \
-        *__pa++ = *__pb;                \
-        *__pb++ = __t;                  \
-    } while (--__n);                    \
-} while (0)
-
 #define swapinit(base, width)                                   \
     ((size_t)(base) % sizeof(long) || (width) % sizeof(long) ?  \
         2 :                                                     \
         (width) == sizeof(long) ? 0 : 1)
 
-#define swapfunc(a, b, size, swaptype)  \
-    if ((swaptype) <= 1)                \
-        swapcode(long, a, b, size);     \
-    else                                \
-        swapcode(char, a, b, size)
-
-#define swap(a, b, size, swaptype)      \
-    if ((swaptype) == 0) {              \
-        long t = *(long *)(a);          \
-        *(long *)(a) = *(long *)(b);    \
-        *(long *)(b) = t;               \
-    } else                              \
-        swapfunc(a, b, size, swaptype)
+#define swapcode(type, a, b, size)      \
+do {                                    \
+    size_t __n = (size) / sizeof(type); \
+    type *__restrict __a = (type *)(a); \
+    type *__restrict __b = (type *)(b); \
+    do {                                \
+        type __t = *__a;                \
+        *(__a++) = *__b;                \
+        *(__b++) = __t;                 \
+    } while (--__n);                    \
+} while (0)
 
 #define vecswap(a, b, size, swaptype)   \
-    if (size)                           \
-        swapfunc(a, b, size, swaptype); \
+    if ((swaptype) <= 1)                \
+        swapcode(uint32_t, a, b, size); \
     else                                \
-        while (0)
+        swapcode(uint8_t, a, b, size)
+
+#define swap(a, b, size, swaptype)           \
+    if ((swaptype) == 0) {                   \
+        uint32_t t = *(uint32_t *)(a);       \
+        *(uint32_t *)(a) = *(uint32_t *)(b); \
+        *(uint32_t *)(b) = t;                \
+    } else                                   \
+        vecswap(a, b, size, swaptype)
 
 #define med3(a, b, c, compare)                                          \
     (compare(a, b) < 0 ?                                                \
@@ -123,75 +119,76 @@ void __cdecl qsort(
 	size_t width,
 	int(__cdecl *compare)(const void *, const void *))
 {
-	char *pa, *pb, *pc, *pd, *pl, *pm, *pn;
-	size_t d;
-	int r;
-	unsigned int swaptype, swap_cnt;
+	unsigned int swaptype;
+	uint8_t      *first, *last, *a, *b, *c, *d;
+	bool         swaped;
+	size_t       n;
+	int          i;
 
 	swaptype = swapinit(base, width);
+	last = (first = (uint8_t *)base) + num * width;
 	for (; ; ) {
 		if (num >= 7) {
-			swap_cnt = 0;
-			pm = (char *)base + (num / 2) * width;
+			swaped = false;
+			b = first + (num / 2) * width;
 			if (num > 7) {
-				pl = base;
-				pn = (char *)base + (num - 1) * width;
+				a = first;
+				c = first + (num - 1) * width;
 				if (num > 40) {
-					d = (num / 8) * width;
-					pl = med3(pl, pl + d, pl + d * 2, compare);
-					pm = med3(pm - d, pm, pm + d, compare);
-					pn = med3(pn - d * 2, pn - d, pn, compare);
+					n = (num / 8) * width;
+					a = med3(a, a + n, a + n * 2, compare);
+					b = med3(b - n, b, b + n, compare);
+					c = med3(c - n * 2, c - n, c, compare);
 				}
-				pm = med3(pl, pm, pn, compare);
+				b = med3(a, b, c, compare);
 			}
-			swap(base, pm, width, swaptype);
-			pa = pb = (char *)base + width;
-			pc = pd = (char *)base + (num - 1) * width;
+			swap(first, b, width, swaptype);
+			a = b = first + width;
+			c = d = first + (num - 1) * width;
 			for (; ; ) {
-				while (pb <= pc && (r = compare(pb, base)) <= 0) {
-					if (r == 0) {
-						swap_cnt = 1;
-						swap(pa, pb, width, swaptype);
-						pa += width;
+				while (b <= c && (i = compare(b, first)) <= 0) {
+					if (i == 0) {
+						swap(a, b, width, swaptype);
+						swaped = true;
+						a += width;
 					}
-					pb += width;
+					b += width;
 				}
-				while (pb <= pc && (r = compare(pc, base)) >= 0) {
-					if (r == 0) {
-						swap_cnt = 1;
-						swap(pc, pd, width, swaptype);
-						pd -= width;
+				while (b <= c && (i = compare(c, first)) >= 0) {
+					if (i == 0) {
+						swap(c, d, width, swaptype);
+						swaped = true;
+						d -= width;
 					}
-					pc -= width;
+					c -= width;
 				}
-				if (pb > pc)
+				if (b > c)
 					break;
-				swap(pb, pc, width, swaptype);
-				swap_cnt = 1;
-				pb += width;
-				pc -= width;
+				swap(b, c, width, swaptype);
+				swaped = true;
+				b += width;
+				c -= width;
 			}
-			if (swap_cnt != 0) {
-				pn = (char *)base + num * width;
-				d = min((size_t)(pa - (char *)base), (size_t)(pb - pa));
-				vecswap(base, pb - d, d, swaptype);
-				d = min((size_t)(pd - pc), pn - pd - width);
-				vecswap(pb, pn - d, d, swaptype);
-				if ((d = pb - pa) > width)
-					qsort(base, d / width, width, compare);
-				if ((d = pd - pc) <= width)
+			if (swaped) {
+				if (n = min((size_t)(a - first), (size_t)(b - a)))
+					vecswap(first, b - n, n, swaptype);
+				if (n = min((size_t)(d - c), last - d - width))
+					vecswap(b, last - n, n, swaptype);
+				if ((n = b - a) > width)
+					qsort(first, n / width, width, compare);
+				if ((n = d - c) <= width)
 					break;
 				/* Iterate rather than recurse to save stack space */
-				base = pn - d;
-				num = d / width;
-				/* qsort(pn - d, d / width, width, compare); */
+				first = last - n;
+				num = n / width;
+				/* qsort(last - n, n / width, width, compare); */
 				continue;
 			}
 		}
 		/* Switch to insertion sort */
-		for (pm = (char *)base + width; pm < (char *)base + num * width; pm += width)
-			for (pl = pm; pl > (char *)base && compare(pl - width, pl) > 0; pl -= width)
-				swap(pl, pl - width, width, swaptype);
+		for (b = first + width; b < last; b += width)
+			for (a = b; a > first && compare(a - width, a) > 0; a -= width)
+				swap(a, a - width, width, swaptype);
 		break;
 	}
 }

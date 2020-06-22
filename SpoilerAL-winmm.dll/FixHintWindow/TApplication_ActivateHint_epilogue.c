c6a3f02bdb23d8d9c2e1455343f64fe78eb430cd
cron <>
2020-06-23 07:00:03 +0900

    Tue Jun 23 06:42:52 JST 2020
      Content-Type: application/zip
      Last-Modified: Mon, 22 Jun 2020 21:42:52 GMT
      Accept-Ranges: bytes
      Content-Length: 1038754
      Date: Mon, 22 Jun 2020 22:00:02 GMT
      Server: LiteSpeed
      Vary: User-Agent
      Alt-Svc: quic=":443"; ma=2592000; v="43,46", h3-Q043=":443"; ma=2592000, h3-Q046=":443"; ma=2592000, h3-Q050=":443"; ma=2592000, h3-25=":443"; ma=2592000, h3-27=":443"; ma=2592000

--- SpoilerAL-winmm.dll/FixHintWindow/TApplication_ActivateHint_epilogue.c
+++ SpoilerAL-winmm.dll/FixHintWindow/TApplication_ActivateHint_epilogue.c
@@ -1,124 +1,142 @@
 #include <windows.h>
-#include <stdbool.h>
+
+#if WM_MOUSELAST < 0x020E
+#undef WM_MOUSELAST
+#define WM_MOUSELAST 0x020E
+#endif
 
 static HANDLE hHook = NULL;
 static HWND   hWnd  = NULL;
 
 #ifndef _M_IX86
-#define PtInRect inline_PtInRect
-static __inline bool PtInRect(CONST RECT *lprc, POINT pt)
-{
-	return  pt.x >= lprc->left && pt.x < lprc->right && pt.y >= lprc->top && pt.y < lprc->bottom;
-}
+#define PtInRect(lprc, pt) ((pt).x >= (lprc)->left && (pt).x < (lprc)->right && (pt).y >= (lprc)->top && (pt).y < (lprc)->bottom)
 
-static LRESULT CALLBACK MouseProc(int nCode, WPARAM wParam, LPARAM lParam)
+static LRESULT CALLBACK GetMsgProc(int nCode, WPARAM wParam, LPARAM lParam)
 {
 	LRESULT lResult;
 	RECT    rc;
-	HANDLE  hPrevHook;
+	HANDLE  hOldHook;
 
 	if (nCode < 0)
-		goto PASS_MESSAGE;
-	if (GetCapture() == hWnd)
-		if (GetWindowRect(hWnd, &rc) && PtInRect(&rc, ((MOUSEHOOKSTRUCT *)lParam)->pt))
-		PASS_MESSAGE:
-			return CallNextHookEx(hHook, nCode, wParam, lParam);
+		goto PASS;
+	if (GetCapture() != hWnd)
+		goto UNHOOK;
+	if ((UINT)(((MSG *)lParam)->message - WM_MOUSEFIRST) < WM_MOUSELAST - WM_MOUSEFIRST + 1)
+		if (GetWindowRect(hWnd, &rc) && PtInRect(&rc, ((MSG *)lParam)->pt))
+			goto PASS;
 		else
-			ReleaseCapture();
-	hPrevHook = hHook;
+			goto RELEASE;
+	else if (((MSG *)lParam)->message == WM_ACTIVATE)
+		goto RELEASE;
+PASS:
+	return CallNextHookEx(hHook, nCode, wParam, lParam);
+
+RELEASE:
+	ReleaseCapture();
+UNHOOK:
+	hOldHook = hHook;
 	hHook = NULL;
-	lResult = CallNextHookEx(hPrevHook, nCode, wParam, lParam);
-	UnhookWindowsHookEx(hPrevHook);
+	lResult = CallNextHookEx(hOldHook, nCode, wParam, lParam);
+	UnhookWindowsHookEx(hOldHook);
 	return lResult;
 }
 #else
-__declspec(naked) static LRESULT CALLBACK MouseProc(int nCode, WPARAM wParam, LPARAM lParam)
+__declspec(naked) static LRESULT CALLBACK GetMsgProc(int nCode, WPARAM wParam, LPARAM lParam)
 {
 	__asm
 	{
-		#define nCode                         (esp + 4)
-		#define wParam                        (esp + 8)
-		#define lParam                        (esp + 12)
-		#define offsetof_MOUSEHOOKSTRUCT_pt_x 0
-		#define offsetof_MOUSEHOOKSTRUCT_pt_y 4
-		#define offsetof_RECT_left            0
-		#define offsetof_RECT_top             4
-		#define offsetof_RECT_right           8
-		#define offsetof_RECT_bottom          12
+	    #define nCode                   (esp + 4)
+	    #define wParam                  (esp + 8)
+	    #define lParam                  (esp + 12)
+	    #define offsetof_MSG_message    4
+	    #define offsetof_MSG_pt_x       20
+	    #define offsetof_MSG_pt_y       24
+	    #define offsetof_RECT_left      0
+	    #define offsetof_RECT_top       4
+	    #define offsetof_RECT_right     8
+	    #define offsetof_RECT_bottom    12
 
-		push    esi
-		push    edi
-		mov     esi, dword ptr [hHook]
-		mov     edi, dword ptr [lParam + 8]
-		cmp     dword ptr [nCode + 8], 0                                    // if (nCode < 0)
-		jl      L1                                                          //     goto PASS_MESSAGE;
-		call    GetCapture                                                  // if (GetCapture() == hWnd)
-		mov     ecx, dword ptr [hWnd]
-		sub     esp, 16
-		cmp     eax, ecx
-		jne     L3
-		push    esp                                                         //     if (GetWindowRect(hWnd, &rc) && PtInRect(&rc, ((MOUSEHOOKSTRUCT *)lParam)->pt))
-		push    eax
-		call    GetWindowRect
-		test    eax, eax
-		jz      L2
-		mov     eax, dword ptr [edi + offsetof_MOUSEHOOKSTRUCT_pt_x]
-		mov     ecx, dword ptr [esp + offsetof_RECT_left]
-		cmp     eax, ecx
-		jl      L2
-		mov     ecx, dword ptr [esp + offsetof_RECT_right]
-		mov     edx, dword ptr [esp + offsetof_RECT_top]
-		cmp     eax, ecx
-		jge     L2
-		mov     eax, dword ptr [edi + offsetof_MOUSEHOOKSTRUCT_pt_y]
-		mov     ecx, dword ptr [esp + offsetof_RECT_bottom]
-		cmp     eax, edx
-		jl      L2
-		cmp     eax, ecx
-		jge     L2
-		add     esp, 16
+	    cmp     dword ptr [nCode], 0                                        // if (nCode < 0)
+	    jl      PASS                                                        //     goto PASS;
+	    call    GetCapture                                                  // if (GetCapture() != hWnd)
+	    mov     ecx, dword ptr [hWnd]                                       //     goto UNHOOK;
+	    mov     edx, dword ptr [lParam]
+	    cmp     eax, ecx
+	    jne     UNHOOK
+	    mov     eax, dword ptr [edx + offsetof_MSG_message]                 // if ((UINT)(((MSG *)lParam)->message - WM_MOUSEFIRST) < WM_MOUSELAST - WM_MOUSEFIRST + 1)
+	    mov     ecx, eax
+	    sub     eax, WM_MOUSEFIRST
+	    cmp     eax, WM_MOUSELAST - WM_MOUSEFIRST + 1
+	    jae     ISACTIVATE
+	    sub     esp, 16                                                     //     if (GetWindowRect(hWnd, &rc) && PtInRect(&rc, ((MSG *)lParam)->pt))
+	    mov     eax, dword ptr [hWnd]                                       //         goto PASS;
+	    push    esp                                                         //     else
+	    push    eax                                                         //         goto RELEASE;
+	    call    GetWindowRect
+	    test    eax, eax
+	    jz      NOWHERE
+	    mov     eax, dword ptr [lParam + 16]
+	    mov     ecx, dword ptr [esp + offsetof_RECT_left]
+	    mov     eax, dword ptr [eax + offsetof_MSG_pt_x]
+	    mov     edx, dword ptr [esp + offsetof_RECT_right]
+	    cmp     eax, ecx
+	    jl      NOWHERE
+	    cmp     eax, edx
+	    jge     NOWHERE
+	    mov     eax, dword ptr [lParam + 16]
+	    mov     ecx, dword ptr [esp + offsetof_RECT_top]
+	    mov     eax, dword ptr [eax + offsetof_MSG_pt_y]
+	    mov     edx, dword ptr [esp + offsetof_RECT_bottom]
+	    cmp     eax, ecx
+	    jl      NOWHERE
+	    cmp     eax, edx
+	    jge     NOWHERE
+	    add     esp, 16
+	    jmp     PASS
 
-		align   16                                                          //     PASS_MESSAGE:
-	L1:
-		mov     eax, esi                                                    //         return CallNextHookEx(hHook, nCode, wParam, lParam);
-		pop     edi
-		pop     esi
-		pop     ecx
-		push    eax
-		push    ecx
-		jmp     CallNextHookEx
+	ISACTIVATE:
+	    cmp     ecx, WM_ACTIVATE                                            // else if (((MSG *)lParam)->message == WM_ACTIVATE)
+	    je      RELEASE                                                     //     goto RELEASE;
+	PASS:                                                                   // PASS:
+	    mov     eax, dword ptr [hHook]                                      //     return CallNextHookEx(hHook, nCode, wParam, lParam);
+	    pop     ecx
+	    push    eax
+	    push    ecx
+	    jmp     CallNextHookEx
 
-		align   16
-	L2:
-		                                                                    //     else
-		call    ReleaseCapture                                              //         ReleaseCapture();
-	L3:
-		mov     eax, dword ptr [wParam + 24]                                // hPrevHook = hHook;
-		mov     ecx, dword ptr [nCode  + 24]
-		add     esp, 16
-		mov     dword ptr [hHook], 0                                        // hHook = NULL;
-		push    edi                                                         // lResult = CallNextHookEx(hPrevHook, nCode, wParam, lParam);
-		push    eax
-		push    ecx
-		push    esi
-		call    CallNextHookEx
-		push    eax
-		push    esi                                                         // UnhookWindowsHookEx(hPrevHook);
-		call    UnhookWindowsHookEx
-		pop     eax                                                         // return lResult;
-		pop     edi
-		pop     esi
-		ret     12
+	NOWHERE:
+	    add     esp, 16
+	RELEASE:                                                                // RELEASE:
+	    call    ReleaseCapture                                              //     ReleaseCapture();
+	UNHOOK:                                                                 // UNHOOK:
+	    mov     eax, dword ptr [lParam]
+	    mov     ecx, dword ptr [wParam]
+	    push    esi
+	    push    eax
+	    mov     eax, dword ptr [nCode + 8]
+	    mov     esi, dword ptr [hHook]                                      // hOldHook = hHook;
+	    push    ecx
+	    push    eax
+	    push    esi
+	    mov     dword ptr [hHook], 0                                        // hHook = NULL;
+	    call    CallNextHookEx                                              // lResult = CallNextHookEx(hOldHook, nCode, wParam, lParam);
+	    push    eax
+	    push    esi                                                         // UnhookWindowsHookEx(hOldHook);
+	    call    UnhookWindowsHookEx
+	    pop     eax                                                         // return lResult;
+	    pop     esi
+	    ret     12
 
-		#undef nCode
-		#undef wParam
-		#undef lParam
-		#undef offsetof_MOUSEHOOKSTRUCT_pt_x
-		#undef offsetof_MOUSEHOOKSTRUCT_pt_y
-		#undef offsetof_RECT_left
-		#undef offsetof_RECT_top
-		#undef offsetof_RECT_right
-		#undef offsetof_RECT_bottom
+	    #undef nCode
+	    #undef wParam
+	    #undef lParam
+	    #undef offsetof_MSG_message
+	    #undef offsetof_MSG_pt_x
+	    #undef offsetof_MSG_pt_y
+	    #undef offsetof_RECT_left
+	    #undef offsetof_RECT_top
+	    #undef offsetof_RECT_right
+	    #undef offsetof_RECT_bottom
 	}
 }
 #endif
@@ -143,12 +161,12 @@ __declspec(naked) void __cdecl TApplication_ActivateHint_epilogue()
 		push    eax                                                         // SetCapture(hWnd);
 		call    SetCapture
 		call    GetCurrentThreadId                                          // hHook = SetWindowsHookExA(
-		push    eax                                                         //     WH_MOUSE,
-		push    0                                                           //     MouseProc,
+		push    eax                                                         //     WH_GETMESSAGE,
+		push    0                                                           //     GetMsgProc,
 		call    GetModuleHandleA                                            //     GetModuleHandleA(NULL),
 		push    eax                                                         //     GetCurrentThreadId());
-		push    offset MouseProc
-		push    WH_MOUSE
+		push    offset GetMsgProc
+		push    WH_GETMESSAGE
 		call    SetWindowsHookExA
 		mov     dword ptr [hHook], eax
 	epilogue:

0be0cd28ff5abf0f3232e4b88cbbe8411396f0c0
cron <>
2020-07-19 05:00:02 +0900

    Sun Jul 19 04:58:43 JST 2020
      Content-Type: application/zip
      Last-Modified: Sat, 18 Jul 2020 19:58:43 GMT
      Accept-Ranges: bytes
      Content-Length: 1046197
      Date: Sat, 18 Jul 2020 20:00:01 GMT
      Server: LiteSpeed
      Vary: User-Agent
      Alt-Svc: quic=":443"; ma=2592000; v="43,46", h3-Q043=":443"; ma=2592000, h3-Q046=":443"; ma=2592000, h3-Q050=":443"; ma=2592000, h3-25=":443"; ma=2592000, h3-27=":443"; ma=2592000

--- SpoilerAL-winmm.dll/HintWindow/HintWindow.c
+++ SpoilerAL-winmm.dll/HintWindow/HintWindow.c
@@ -1,13 +1,18 @@
 #include "HintWindow.h"
 #include "commctrl.h"
+#include "intrinsic.h"
 #define USING_NAMESPACE_BCB6_STD
 #include "TApplication.h"
 #include "TWinControl.h"
+#include "TDrawGrid.h"
+
+#define EqualPoint(lppt1, lppt2) (!(((lppt1)->x ^ (lppt2)->x) | ((lppt1)->y ^ (lppt2)->y)))
 
 static HWND      hToolTip = NULL;
 static TOOLINFOA ti = { sizeof(TOOLINFOA), TTF_IDISHWND | TTF_SUBCLASS };
 static HHOOK     hHook = NULL;
-static DWORD     dwTrackPos;
+static POINT     ptTrack;
+static TDrawGrid *DrawGrid;
 
 static LRESULT CALLBACK CallWndRetProc(int nCode, WPARAM wParam, LPARAM lParam);
 
@@ -51,10 +56,11 @@ void __cdecl DestroyHintWindow()
 void __fastcall TApplication_ActivateHint(TApplication *this, LPPOINT CursorPos)
 {
 	HWND hWnd;
+	char lpClassName[16];
 
 	if (hHook || !this->Control || !hToolTip)
 		return;
-	dwTrackPos = GetMessagePos();
+	ptTrack = *CursorPos;
 	ti.lpszText = (LPSTR)this->Control->Hint;
 	if ((hWnd = TWinControl_GetHandle(this->Control)) == (HWND)ti.uId)
 	{
@@ -67,19 +73,37 @@ void __fastcall TApplication_ActivateHint(TApplication *this, LPPOINT CursorPos)
 		ti.uId = (UINT_PTR)hWnd;
 		SendMessageA(hToolTip, TTM_ADDTOOLA, 0, (LPARAM)&ti);
 	}
+	DrawGrid = NULL;
+	if (GetClassNameA((HWND)ti.uId, lpClassName, _countof(lpClassName)) == 9 &&
+		*(LPDWORD)&lpClassName[0] == BSWAP32('TDra') &&
+		*(LPDWORD)&lpClassName[4] == BSWAP32('wGri') &&
+		*(LPWORD )&lpClassName[8] == BSWAP16('d\0'))
+	{
+		ScreenToClient((HWND)ti.uId, &ptTrack);
+		TDrawGrid_MouseToCell(DrawGrid = (TDrawGrid *)this->Control, ptTrack.x, ptTrack.y, &ptTrack.x, &ptTrack.y);
+	}
 	hHook = SetWindowsHookExA(WH_CALLWNDPROCRET, CallWndRetProc, NULL, GetCurrentThreadId());
 }
 
 static LRESULT CALLBACK CallWndRetProc(int nCode, WPARAM wParam, LPARAM lParam)
 {
 	LRESULT lResult;
+	POINT   ptCursor;
 
 	lResult = CallNextHookEx(hHook, nCode, wParam, lParam);
-	if (nCode >= 0 && (GetMessagePos() != dwTrackPos || !IsWindowVisible((HWND)ti.uId)))
+	if (nCode >= 0 && GetCursorPos(&ptCursor))
 	{
-		UnhookWindowsHookEx(hHook);
-		SendMessageA(hToolTip, TTM_TRACKACTIVATE, FALSE, (LPARAM)&ti);
-		hHook = NULL;
+		if (DrawGrid)
+			if (ScreenToClient((HWND)ti.uId, &ptCursor))
+				TDrawGrid_MouseToCell(DrawGrid, ptCursor.x, ptCursor.y, &ptCursor.x, &ptCursor.y);
+			else
+				ptCursor.y = ptCursor.x = -1;
+		if (!EqualPoint(&ptCursor, &ptTrack) || !IsWindowVisible((HWND)ti.uId))
+		{
+			UnhookWindowsHookEx(hHook);
+			SendMessageA(hToolTip, TTM_TRACKACTIVATE, FALSE, (LPARAM)&ti);
+			hHook = NULL;
+		}
 	}
 	return lResult;
 }

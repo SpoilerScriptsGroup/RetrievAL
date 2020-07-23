4b5794b71ec5f807854cf0b45f6589f7101ab1cd
cron <>
2020-07-23 22:00:01 +0900

    Thu Jul 23 21:55:32 JST 2020
      Content-Type: application/zip
      Last-Modified: Thu, 23 Jul 2020 12:55:32 GMT
      Accept-Ranges: bytes
      Content-Length: 1045928
      Date: Thu, 23 Jul 2020 13:00:01 GMT
      Server: LiteSpeed
      Vary: User-Agent
      Alt-Svc: quic=":443"; ma=2592000; v="43,46", h3-Q043=":443"; ma=2592000, h3-Q046=":443"; ma=2592000, h3-Q050=":443"; ma=2592000, h3-25=":443"; ma=2592000, h3-27=":443"; ma=2592000

--- SpoilerAL-winmm.dll/HintWindow/HintWindow.c
+++ SpoilerAL-winmm.dll/HintWindow/HintWindow.c
@@ -12,6 +12,7 @@ static HWND      hToolTip = NULL;
 static TOOLINFOA ti = { sizeof(TOOLINFOA), TTF_IDISHWND | TTF_SUBCLASS };
 static HHOOK     hHook = NULL;
 static POINT     ptTrack;
+static BOOL      IsDrawGrid;
 static TDrawGrid *DrawGrid;
 
 static LRESULT CALLBACK CallWndRetProc(int nCode, WPARAM wParam, LPARAM lParam);
@@ -73,12 +74,13 @@ void __fastcall TApplication_ActivateHint(TApplication *this, LPPOINT CursorPos)
 		ti.uId = (UINT_PTR)hWnd;
 		SendMessageA(hToolTip, TTM_ADDTOOLA, 0, (LPARAM)&ti);
 	}
-	DrawGrid = NULL;
+	IsDrawGrid = 0;
 	if (GetClassNameA((HWND)ti.uId, lpClassName, _countof(lpClassName)) == 9 &&
 		*(LPDWORD)&lpClassName[0] == BSWAP32('TDra') &&
 		*(LPDWORD)&lpClassName[4] == BSWAP32('wGri') &&
 		*(LPWORD )&lpClassName[8] == BSWAP16('d\0'))
 	{
+		IsDrawGrid = -1;
 		ScreenToClient((HWND)ti.uId, &ptTrack);
 		TDrawGrid_MouseToCell(DrawGrid = (TDrawGrid *)this->Control, ptTrack.x, ptTrack.y, &ptTrack.x, &ptTrack.y);
 	}
@@ -91,14 +93,14 @@ static LRESULT CALLBACK CallWndRetProc(int nCode, WPARAM wParam, LPARAM lParam)
 	POINT   ptCursor;
 
 	lResult = CallNextHookEx(hHook, nCode, wParam, lParam);
-	if (nCode >= 0 && GetCursorPos(&ptCursor))
+	if (nCode >= 0)
 	{
-		if (DrawGrid)
-			if (ScreenToClient((HWND)ti.uId, &ptCursor))
+		BOOL bSuccess;
+
+		if ((bSuccess = GetCursorPos(&ptCursor)) & IsDrawGrid)
+			if (bSuccess = ScreenToClient((HWND)ti.uId, &ptCursor))
 				TDrawGrid_MouseToCell(DrawGrid, ptCursor.x, ptCursor.y, &ptCursor.x, &ptCursor.y);
-			else
-				ptCursor.y = ptCursor.x = -1;
-		if (!EqualPoint(&ptCursor, &ptTrack) || !IsWindowVisible((HWND)ti.uId))
+		if (!bSuccess || !EqualPoint(&ptCursor, &ptTrack) || !IsWindowVisible((HWND)ti.uId))
 		{
 			UnhookWindowsHookEx(hHook);
 			SendMessageA(hToolTip, TTM_TRACKACTIVATE, FALSE, (LPARAM)&ti);

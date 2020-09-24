f4f1f87a0c13418166625b9a501b42f47a87089c
cron <>
2020-09-24 21:00:02 +0900

    Thu Sep 24 20:19:57 JST 2020
      Content-Type: application/zip
      Last-Modified: Thu, 24 Sep 2020 11:19:57 GMT
      Accept-Ranges: bytes
      Content-Length: 1074062
      Date: Thu, 24 Sep 2020 12:00:01 GMT
      Server: LiteSpeed
      Vary: User-Agent
      Alt-Svc: quic=":443"; ma=2592000; v="43,46", h3-Q043=":443"; ma=2592000, h3-Q046=":443"; ma=2592000, h3-Q050=":443"; ma=2592000, h3-25=":443"; ma=2592000, h3-27=":443"; ma=2592000

--- SpoilerAL-winmm.dll/crt/msvc/memcmp.asm
+++ SpoilerAL-winmm.dll/crt/msvc/memcmp.asm
@@ -28,41 +28,39 @@ L2:
 	sub     eax, 4
 	jae     L1
 	add     eax, 4
-	jz      L4
+	jz      L5
 L3:
 	mov     cl, byte ptr [esi]
 	mov     dl, byte ptr [edi]
 	cmp     cl, dl
-	jne     L5
+	jne     L4
 	dec     eax
-	jz      L4
+	jz      L5
 	mov     cl, byte ptr [esi + 1]
 	mov     dl, byte ptr [edi + 1]
 	cmp     cl, dl
-	jne     L5
+	jne     L4
 	dec     eax
-	jz      L4
+	jz      L5
 	mov     cl, byte ptr [esi + 2]
 	mov     dl, byte ptr [edi + 2]
 	cmp     cl, dl
-	jne     L5
+	jne     L4
 	dec     eax
-	jz      L4
+	jz      L5
 	mov     cl, byte ptr [esi + 3]
 	mov     dl, byte ptr [edi + 3]
 	cmp     cl, dl
-	jne     L5
-	xor     eax, eax
 L4:
+	sbb     eax, eax
 	pop     edi
+	or      eax, 1
 	pop     esi
 	ret
 
 	align   16
 L5:
-	sbb     eax, eax
 	pop     edi
-	or      eax, 1
 	pop     esi
 	ret
 

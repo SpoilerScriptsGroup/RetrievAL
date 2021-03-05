19b07d9eb7dd845263bd0b1729d1db925a258524
cron <>
2021-03-05 21:00:03 +0900

    Fri Mar  5 20:44:00 JST 2021
    Content-Type: application/zip
    Last-Modified: Fri, 05 Mar 2021 11:44:00 GMT
    Accept-Ranges: bytes
    Content-Length: 1188765
    Date: Fri, 05 Mar 2021 12:00:01 GMT
    Server: LiteSpeed
    Vary: User-Agent

--- SpoilerAL-winmm.dll/datetime.h
+++ SpoilerAL-winmm.dll/datetime.h
@@ -21,33 +21,7 @@
 #define __TIME_SS__           00
 #define __TIME_FFF__          000
 
-#define __DATETIME_TIME_T__ (                                                       \
-    (                                                                               \
-          (__DATE_YEAR__ - (__DATE_MONTH__ < 3)) * 365 - 365                        \
-        + (__DATE_YEAR__ - (__DATE_MONTH__ < 3)) / 4                                \
-        - (__DATE_YEAR__ - (__DATE_MONTH__ < 3)) / 100                              \
-        + (__DATE_YEAR__ - (__DATE_MONTH__ < 3)) / 400                              \
-        + ((__DATE_MONTH__ + (__DATE_MONTH__ < 3) * 12) * 979 - 1033) / 32          \
-        + __DATE_DAY__ - 1                                                          \
-        - ((1970 - 1) * 365 + (1970 - 1) / 4 - (1970 - 1) / 100 + (1970 - 1) / 400) \
-    ) * 86400                                                                       \
-    + __TIME_HOUR__ * 3600                                                          \
-    + __TIME_MINUTE__ * 60                                                          \
-    + __TIME_SECOND__)
-
-#define __DATETIME_FILETIME__ (                                                     \
-    (                                                                               \
-          (__DATE_YEAR__ - (__DATE_MONTH__ < 3)) * 365 - 365                        \
-        + (__DATE_YEAR__ - (__DATE_MONTH__ < 3)) / 4                                \
-        - (__DATE_YEAR__ - (__DATE_MONTH__ < 3)) / 100                              \
-        + (__DATE_YEAR__ - (__DATE_MONTH__ < 3)) / 400                              \
-        + ((__DATE_MONTH__ + (__DATE_MONTH__ < 3) * 12) * 979 - 1033) / 32          \
-        + __DATE_DAY__ - 1                                                          \
-        - ((1601 - 1) * 365 + (1601 - 1) / 4 - (1601 - 1) / 100 + (1601 - 1) / 400) \
-    ) * 864000000000                                                                \
-    + __TIME_HOUR__ * 36000000000                                                   \
-    + __TIME_MINUTE__ * 600000000                                                   \
-    + __TIME_SECOND__ * 10000000                                                    \
-    + __TIME_MILLISECOND__ * 10000)
+#define __DATETIME_TIME_T__   0x0000000000000000
+#define __DATETIME_FILETIME__ 0x019DB1DED53E8000
 
 #endif	// _DATETIME_H_
--- SpoilerAL-winmm.dll/datetime_h.bat
+++ SpoilerAL-winmm.dll/datetime_h.bat
@@ -5,18 +5,25 @@ if "%~1" == "" (
 ) else (
 	set FileName=%1
 )
-:LOOPHEAD
-set current_date=%date%
-set current_time=%time%
-if not "%current_date%" == "%date%" goto :LOOPHEAD
-set date_yyyy=%current_date:~0,4%
-set date_mm=%current_date:~5,2%
-set date_dd=%current_date:~8,2%
-set time_hh=%current_time:~0,2%
-set time_hh=%time_hh: =0%
-set time_mm=%current_time:~3,2%
-set time_ss=%current_time:~6,2%
-set time_fff=%current_time:~9,2%0
+set env=
+set command=$d = Get-Date;
+set command=%command% $d = Get-Date;
+set command=%command% $ft = $d.Ticks - 0x0701CE1722770000;
+set command=%command% $tm = [uint64]($ft / 10000000) - 11644473600;
+set command=%command% Write-Host(\"{0:d4}/{1:d2}/{2:d2} {3:d2}:{4:d2}:{5:d2}.{6:d3} 0x{7:X16} 0x{8:X16}\" -f $d.Year, $d.Month, $d.Day, $d.Hour, $d.Minute, $d.Second, $d.Millisecond, $tm, $ft);
+for /f "usebackq tokens=1-4 delims= " %%a in (`powershell.exe -Command "%command%"`) do (
+	set d=%%a
+	set t=%%b
+	set tm=%%c
+	set ft=%%d
+)
+set date_yyyy=%d:~0,4%
+set date_mm=%d:~5,2%
+set date_dd=%d:~8,2%
+set time_hh=%t:~0,2%
+set time_mm=%t:~3,2%
+set time_ss=%t:~6,2%
+set time_fff=%t:~9,3%
 set /a date_year=1%date_yyyy%-10000
 set /a date_month=1%date_mm%-100
 set /a date_day=1%date_dd%-100
@@ -47,34 +54,8 @@ set /a time_millisecond=1%time_fff%-1000
 >>%FileName% echo #define __TIME_SS__           %time_ss%
 >>%FileName% echo #define __TIME_FFF__          %time_fff%
 >>%FileName% echo.
->>%FileName% echo #define __DATETIME_TIME_T__ (                                                       \
->>%FileName% echo     (                                                                               \
->>%FileName% echo           (__DATE_YEAR__ - (__DATE_MONTH__ ^< 3)) * 365 - 365                        \
->>%FileName% echo         + (__DATE_YEAR__ - (__DATE_MONTH__ ^< 3)) / 4                                \
->>%FileName% echo         - (__DATE_YEAR__ - (__DATE_MONTH__ ^< 3)) / 100                              \
->>%FileName% echo         + (__DATE_YEAR__ - (__DATE_MONTH__ ^< 3)) / 400                              \
->>%FileName% echo         + ((__DATE_MONTH__ + (__DATE_MONTH__ ^< 3) * 12) * 979 - 1033) / 32          \
->>%FileName% echo         + __DATE_DAY__ - 1                                                          \
->>%FileName% echo         - ((1970 - 1) * 365 + (1970 - 1) / 4 - (1970 - 1) / 100 + (1970 - 1) / 400) \
->>%FileName% echo     ) * 86400                                                                       \
->>%FileName% echo     + __TIME_HOUR__ * 3600                                                          \
->>%FileName% echo     + __TIME_MINUTE__ * 60                                                          \
->>%FileName% echo     + __TIME_SECOND__)
->>%FileName% echo.
->>%FileName% echo #define __DATETIME_FILETIME__ (                                                     \
->>%FileName% echo     (                                                                               \
->>%FileName% echo           (__DATE_YEAR__ - (__DATE_MONTH__ ^< 3)) * 365 - 365                        \
->>%FileName% echo         + (__DATE_YEAR__ - (__DATE_MONTH__ ^< 3)) / 4                                \
->>%FileName% echo         - (__DATE_YEAR__ - (__DATE_MONTH__ ^< 3)) / 100                              \
->>%FileName% echo         + (__DATE_YEAR__ - (__DATE_MONTH__ ^< 3)) / 400                              \
->>%FileName% echo         + ((__DATE_MONTH__ + (__DATE_MONTH__ ^< 3) * 12) * 979 - 1033) / 32          \
->>%FileName% echo         + __DATE_DAY__ - 1                                                          \
->>%FileName% echo         - ((1601 - 1) * 365 + (1601 - 1) / 4 - (1601 - 1) / 100 + (1601 - 1) / 400) \
->>%FileName% echo     ) * 864000000000                                                                \
->>%FileName% echo     + __TIME_HOUR__ * 36000000000                                                   \
->>%FileName% echo     + __TIME_MINUTE__ * 600000000                                                   \
->>%FileName% echo     + __TIME_SECOND__ * 10000000                                                    \
->>%FileName% echo     + __TIME_MILLISECOND__ * 10000)
+>>%FileName% echo #define __DATETIME_TIME_T__   %tm%
+>>%FileName% echo #define __DATETIME_FILETIME__ %ft%
 >>%FileName% echo.
 >>%FileName% echo #endif	// _DATETIME_H_
 endlocal

b32dab9b2de7c29939ef12bcc2bd2c8b8cdbf491
cron <>
2021-03-09 22:00:03 +0900

    Tue Mar  9 21:44:34 JST 2021
    Content-Type: application/zip
    Last-Modified: Tue, 09 Mar 2021 12:44:34 GMT
    Accept-Ranges: bytes
    Content-Length: 1187936
    Date: Tue, 09 Mar 2021 13:00:01 GMT
    Server: LiteSpeed
    Vary: User-Agent

--- SpoilerAL-winmm.dll/datetime.h
+++ SpoilerAL-winmm.dll/datetime.h
@@ -8,18 +8,26 @@
 #define __DATE_YEAR__         1970
 #define __DATE_MONTH__        1
 #define __DATE_DAY__          1
-#define __TIME_HOUR__         0
+#define __TIME_HOUR__         9
 #define __TIME_MINUTE__       0
 #define __TIME_SECOND__       0
 #define __TIME_MILLISECOND__  0
+#define __TIME_MICROSECOND__  0
+#define __TIME_NANOSECOND__   0
 
 #define __DATE_YYYY__         1970
 #define __DATE_MM__           01
 #define __DATE_DD__           01
-#define __TIME_HH__           00
+#define __TIME_HH__           09
 #define __TIME_MM__           00
 #define __TIME_SS__           00
+#define __TIME_F__            0
+#define __TIME_FF__           00
 #define __TIME_FFF__          000
+#define __TIME_FFFF__         0000
+#define __TIME_FFFFF__        00000
+#define __TIME_FFFFFF__       000000
+#define __TIME_FFFFFFF__      0000000
 
 #define __DATETIME_TIME_T__   0x0000000000000000
 #define __DATETIME_FILETIME__ 0x019DB1DED53E8000
